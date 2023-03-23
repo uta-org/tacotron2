315a25c831a8de6552d79d40020cfb4807b09d6a
z3nth10n <z3nth10n@gmail.com>
2023-03-23 19:20:05 +0100

    solve problem on stft with pad_center VII

--- layers.py
+++ layers.py
@@ -48,10 +48,10 @@ class TacotronSTFT(torch.nn.Module):
         self.sampling_rate = sampling_rate
         self.stft_fn = STFT(filter_length, hop_length, win_length)
         # TODO
-        # mel_basis = librosa_mel_fn(
-        #     sampling_rate, filter_length, n_mel_channels, mel_fmin, mel_fmax)
-        # mel_basis = torch.from_numpy(mel_basis).float()
-        # self.register_buffer('mel_basis', mel_basis)
+        mel_basis = librosa_mel_fn(
+            sr=sampling_rate, n_fft=filter_length, n_mels=n_mel_channels, fmin=mel_fmin, fmax=mel_fmax)
+        mel_basis = torch.from_numpy(mel_basis).float()
+        self.register_buffer('mel_basis', mel_basis)
 
     def spectral_normalize(self, magnitudes):
         output = dynamic_range_compression(magnitudes)
